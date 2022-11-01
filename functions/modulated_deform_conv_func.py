c38b29cbda9f286376e0b6049eac226c3c33a801
yukistavailable <yukitatsu_0817@yahoo.co.jp>
2022-11-02 04:38:32 +0900

    fix bug

--- functions/modulated_deform_conv_func.py
+++ functions/modulated_deform_conv_func.py
@@ -12,10 +12,22 @@ from torch.autograd.function import once_differentiable
 
 import DCN
 
+
 class ModulatedDeformConvFunction(Function):
     @staticmethod
-    def forward(ctx, input, offset, mask, weight, bias,
-                stride, padding, dilation, groups, deformable_groups, im2col_step):
+    def forward(
+            ctx,
+            input,
+            offset,
+            mask,
+            weight,
+            bias,
+            stride,
+            padding,
+            dilation,
+            groups,
+            deformable_groups,
+            im2col_step):
         ctx.stride = _pair(stride)
         ctx.padding = _pair(padding)
         ctx.dilation = _pair(dilation)
@@ -23,15 +35,23 @@ class ModulatedDeformConvFunction(Function):
         ctx.groups = groups
         ctx.deformable_groups = deformable_groups
         ctx.im2col_step = im2col_step
-        output = DCN.modulated_deform_conv_forward(input, weight, bias,
-                                         offset, mask,
-                                         ctx.kernel_size[0], ctx.kernel_size[1],
-                                         ctx.stride[0], ctx.stride[1],
-                                         ctx.padding[0], ctx.padding[1],
-                                         ctx.dilation[0], ctx.dilation[1],
-                                         ctx.groups,
-                                         ctx.deformable_groups,
-                                         ctx.im2col_step)
+        output = DCN.modulated_deform_conv2d_forward(
+            input,
+            weight,
+            bias,
+            offset,
+            mask,
+            ctx.kernel_size[0],
+            ctx.kernel_size[1],
+            ctx.stride[0],
+            ctx.stride[1],
+            ctx.padding[0],
+            ctx.padding[1],
+            ctx.dilation[0],
+            ctx.dilation[1],
+            ctx.groups,
+            ctx.deformable_groups,
+            ctx.im2col_step)
         ctx.save_for_backward(input, offset, mask, weight, bias)
         return output
 
@@ -41,16 +61,16 @@ class ModulatedDeformConvFunction(Function):
         input, offset, mask, weight, bias = ctx.saved_tensors
         grad_input, grad_offset, grad_mask, grad_weight, grad_bias = \
             DCN.modulated_deform_conv_backward(input, weight,
-                                     bias,
-                                     offset, mask,
-                                     grad_output,
-                                     ctx.kernel_size[0], ctx.kernel_size[1],
-                                     ctx.stride[0], ctx.stride[1],
-                                     ctx.padding[0], ctx.padding[1],
-                                     ctx.dilation[0], ctx.dilation[1],
-                                     ctx.groups,
-                                     ctx.deformable_groups,
-                                     ctx.im2col_step)
+                                               bias,
+                                               offset, mask,
+                                               grad_output,
+                                               ctx.kernel_size[0], ctx.kernel_size[1],
+                                               ctx.stride[0], ctx.stride[1],
+                                               ctx.padding[0], ctx.padding[1],
+                                               ctx.dilation[0], ctx.dilation[1],
+                                               ctx.groups,
+                                               ctx.deformable_groups,
+                                               ctx.im2col_step)
 
         return grad_input, grad_offset, grad_mask, grad_weight, grad_bias,\
             None, None, None, None, None, None
